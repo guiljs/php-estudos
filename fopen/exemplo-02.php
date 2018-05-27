b75628bece6c70e5997f7ae03563dd16cbe03d06
Guilherme de Jesus Santos <guilhermejs@hotmail.com>
2018-05-27 12:47:28 -0300

    Organização do código

--- fopen/exemplo-02.php
+++ fopen/exemplo-02.php
@@ -1,29 +1,47 @@
 <?php
 require_once("config.php");
 
-$sql = new Sql();
+main();
 
-$usuarios = $sql->select("select * from tb_usuarios order by deslogin");
+?>
 
-$headers = array();
+<?php
 
-foreach ($usuarios[0] as $key => $value) {
-  array_push($headers, ucfirst($key));
+function main()
+{
+  $file=fopen("usuarios.csv","w+");
+  $sql = new Sql();
+
+  $usuarios = $sql->select("select * from tb_usuarios order by deslogin");
+
+  geraHeaders($usuarios,$file);
+
+  geraLinhas($usuarios,$file);
+
+  fclose($file);
+
+}
+function geraHeaders($usuarios,$file)
+{
+  $headers = array();
+
+  foreach ($usuarios[0] as $key => $value) {
+    array_push($headers, ucfirst($key));
+  }
+
+  fwrite($file,implode(",",$headers)."\r\n");
 }
 
-$file=fopen("usuarios.csv","w+");
+function geraLinhas($usuarios,$file)
+{
+  foreach ($usuarios as $row) {
+    $data = array();
+    //Foreach de linha
+    foreach ($row as $key => $value) {
+      array_push($data,$value);
+    }//foreach de coluna
+    fwrite($file, implode(",",$data)."\r\n");
+  }//End foreach de linha
+}
 
-fwrite($file,implode(",",$headers)."\r\n");
-
-foreach ($usuarios as $row) {
-  $data = array();
-//Foreach de linha
-  foreach ($row as $key => $value) {
-    array_push($data,$value);
-  }//foreach de coluna
-  fwrite($file, implode(",",$data)."\r\n");
-}//End foreach de linha
-
-fclose($file);
-
- ?>
+?>
