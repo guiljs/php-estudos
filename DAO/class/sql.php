639137248b998236050c7d463cb3e657f43f5da8
Guilherme de Jesus Santos <guilhermejs@hotmail.com>
2018-05-26 22:35:15 -0300

    Organizando com DAO

---
+++ DAO/class/sql.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ */
+class Sql extends PDO
+{
+  private $conn;
+  public function __construct()
+  {
+    $this->conn = new PDO("mysql:host=127.0.0.1;dbname=dbphp7","root","");
+  }
+
+  private function setParams($statement, $parameters=array()){
+    foreach ($parameters as $key => $value) {
+      $this->setParam($key,$value);
+    }
+  }
+
+  private function setParam($statement, $key, $value){
+    $statement->bindParam($key,$value);
+  }
+
+  public function query($rowQuery, $params = array()){
+    $stmt = $this->conn->prepare($rowQuery);
+    $this->setParams($stmt,$params);
+    $stmt->execute();
+    return $stmt;
+  }
+
+  public function select($rowQuery,$params=array()):array
+  {
+    $stmt = $this->query($rowQuery,$params);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
+
+ ?>
