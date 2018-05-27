19ec75ee445c5010a27ac27eb6db9106dd3654bd
Guilherme de Jesus Santos <guilhermejs@hotmail.com>
2018-05-26 23:46:14 -0300

    Melhorias na classe Usuario e testes no index.php

--- DAO/class/usuario.php
+++ DAO/class/usuario.php
@@ -63,6 +63,40 @@ class Usuario
       $this->setDtcadastro(new DateTime($row['dtcadastro']));
     }
   }
+
+  public static function getList()
+  {
+    $sql = new Sql();
+    return $sql->select("Select * from tb_usuarios ORDER BY deslogin");
+  }
+
+  public static function search($login)
+  {
+    $sql= new Sql();
+    return $sql->select("Select * from tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+      ':SEARCH'=>"%".$login."%"
+    ));
+  }
+
+  public function login($login,$password)
+  {
+    $sql=new Sql();
+    $results = $sql->select("SELECT * FROM tb_usuarios where deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+      ":LOGIN"=>$login,
+      ":PASSWORD"=>$password
+    ));
+
+    if (isset($results[0])) {
+      $row = $results[0];
+      $this->setIdUsuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+    } else {
+      throw new Exception("Login e/ou senha inválidos.");
+    }
+  }
+
   public function __toString()
   {
     return json_encode(array(
--- DAO/index.php
+++ DAO/index.php
@@ -2,11 +2,11 @@
 
 require_once("config.php");
 
-$user1 = new Usuario();
-
-$user1->loadById(4);
-
-echo $user1;
+// $user1 = new Usuario();
+//
+// $user1->loadById(4);
+//
+// echo $user1;
 
 // echo "<hr>";
 //
@@ -19,4 +19,14 @@ echo $user1;
 //   echo "<hr>";
 // }
 
+// $lista = Usuario::getList();
+//
+// echo json_encode($lista);
+
+// $search = Usuario::search("he");
+// echo json_encode($search);
+
+$usuario = new Usuario();
+$usuario->login("gjsantos","senha123");
+echo $usuario;
  ?>
