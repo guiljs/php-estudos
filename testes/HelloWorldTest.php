8df8fbd55b656230c4bb2bf34d3eb2e132617186
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-05-30 15:10:52 -0300

    Create HelloWorldTest.php

---
+++ testes/HelloWorldTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class HelloWorldTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function setUp()
+    {
+       
+    }
+
+    public function tearDown()
+    {
+       
+    }
+
+    public function testHelloWorld()
+    {
+        $this->assertEquals('Hello World', 'Hello World');
+    }
+}
