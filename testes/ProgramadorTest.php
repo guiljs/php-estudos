f2dd83cdee64b9a6ac724eba16d7297aac5b9aa9
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-06-05 09:59:13 -0300

    Create ProgramadorTest.php
    Um teste com a classe programador

---
+++ testes/ProgramadorTest.php
@@ -0,0 +1,28 @@
+<?php
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+require_once(../poo/exemplo-01.php);
+use poo/exemplo-01.php/programador;
+
+class ProgramadorTest extends \PHPUnit\Framework\TestCase
+{
+    public function setUp()
+    {
+       
+    }
+    public function tearDown()
+    {
+       
+    }
+    public function testGetLinguagem()
+    {
+        $programador = new Programador("Heitor",4);
+        $programador->setSexo("masculino");
+        $programador->setLinguagemProgramacao("javascript");
+    
+        $this->assertEquals($programador->getLinguagemProgramacao(), 'javascript');
+    }
+}
